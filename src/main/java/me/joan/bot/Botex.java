0ccb89e95ae044dc776c875174962fcc9fcf9599
Joan-GQ <86327859+Joan-GQ@users.noreply.github.com>
2022-03-27 07:22:56 -0300

    Update Botex.java

--- src/main/java/me/joan/bot/Botex.java
+++ src/main/java/me/joan/bot/Botex.java
@@ -1,4 +1,25 @@
 package me.joan.bot;
 
+import me.joan.Main;
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.*;
+
+import java.util.*;
+import java.time.*;
+
 public class Botex {
+    public Main main;
+    private JDA jda;
+
+    private final String[] channel_names = {"-reglas"};
+    private final String[] strings;
+    private final ArrayList<TextChannel> channels;
+    public static final String PREFIX = "x!"+" ";
+
+    public Botex(Main main, String key, String[] strings) {
+        this.main = main;
+        this.channels = new ArrayList<TextChannel>();
+        this.strings = strings;
+    }
 }
